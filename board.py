a6da476046c366d6b4252c6d756a79793f85ce94
richelbilderbeek <richel@richelbilderbeek.nl>
2020-12-20 10:50:58 +0100

    Add Board

---
+++ board.py
@@ -0,0 +1,13 @@
+class Board:
+    # A Mastermind board
+
+    n_cols = 4
+    # Number of columns where a player can place a peg
+
+    n_rows = 10 # Let Kirsten check
+    # Equals the number of turns a player has
+
+    def print_me(self):
+    # Show the board as text one day
+        print("Me: ", self.n_cols, self.n_rows)
+
