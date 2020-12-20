9de79034f6a7ce6d958ce3ca1e462e2f02e0da6a
richelbilderbeek <richel@richelbilderbeek.nl>
2020-12-20 09:20:04 +0100

    Add minimal program

--- main.py
+++ main.py
@@ -1,7 +1,31 @@
-# This is a start
+# import the pygame module, so you can use it
+import pygame
+ 
+# define a main function
 def main():
-    print("hello world!")
-
-if __name__ == "__main__":
+     
+    # initialize the pygame module
+    pygame.init()
+    pygame.display.set_caption("kiri")
+     
+    # create a surface on screen that has the size of 240 x 180
+    screen = pygame.display.set_mode((240,180))
+     
+    # define a variable to control the main loop
+    running = True
+     
+    # main loop
+    while running:
+        # event handling, gets all event from the event queue
+        for event in pygame.event.get():
+            # only do something if the event is of type QUIT
+            if event.type == pygame.QUIT:
+                # change the value to False, to exit the main loop
+                running = False
+     
+     
+# run the main function only if this module is executed as the main script
+# (if you import this as a module then nothing is executed)
+if __name__=="__main__":
+    # call the main function
     main()
-
