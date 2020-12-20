2e5459ec5a0c963242c8eb00c84480a7f750f720
richelbilderbeek <richel@richelbilderbeek.nl>
2020-12-20 13:23:34 +0100

    Expose #6

--- peg_colors.py
+++ peg_colors.py
@@ -21,3 +21,13 @@ def test_peg_colors():
         n_expected = get_n_player_peg_colors()
         n_actual = len(get_player_peg_colors())
         assert n_expected == n_actual, "There must be 6 peg colors"
+
+    if False:
+        # Issue 6
+        assert "red" in get_player_peg_colors(), "Red is a peg color"
+        assert "yellow" in get_player_peg_colors(), "Yellow is a peg color"
+        assert "green" in get_player_peg_colors(), "Green is a peg color"
+        assert "cyan" in get_player_peg_colors(), "Cyan is a peg color"
+        assert "blue" in get_player_peg_colors(), "Blue is a peg color"
+        assert "magenta" in get_player_peg_colors(), "Magenta is a peg color"
+
