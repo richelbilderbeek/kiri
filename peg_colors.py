be44e4cdaf1967f09dfa83eb052ad43ba16aaa08
Kirsten Ferwerda <kirstenferwerda@hotmail.com>
2020-12-20 14:24:07 +0100

    Fix #5 and added return

--- peg_colors.py
+++ peg_colors.py
@@ -3,8 +3,7 @@ def get_n_player_peg_colors():
 
 
 def get_player_peg_colors():
-    # Return all six player peg colors
-    "Ultraviolet"
+    return "red", "yellow", "green", "cyan", "blue", "magenta"
 
 
 def test_peg_colors():
@@ -13,8 +12,7 @@ def test_peg_colors():
     if True:
         assert get_n_player_peg_colors() == 6, "The player has 6 peg colors"
 
-    if False:
-        # Issue 5
+    if True:
         n_expected = get_n_player_peg_colors()
         n_actual = len(get_player_peg_colors())
         assert n_expected == n_actual, "There must be 6 peg colors"
