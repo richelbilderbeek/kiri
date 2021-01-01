2e3605742c760717999480b92eca88f20d3465a0
richelbilderbeek <richel@richelbilderbeek.nl>
2021-01-01 12:09:11 +0100

    Expose #13, #14, #15, #16, #17

--- main.py
+++ main.py
@@ -5,12 +5,14 @@ os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"  # noqa due to PyGame
 import pygame
 from board import *
 from peg_colors import *
+from game import *
 
 
 # define the main main testing function
 def test():
     test_peg_colors()
     test_board()
+    test_game()
 
 
 # define a main function
